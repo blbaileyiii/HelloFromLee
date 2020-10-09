f35c2a9ce4a878159a9f0931d6f0daca227612df
blbaileyiii <blbaileyiii.naa@gmail.com>
2020-10-09 15:45:13 -0700

    Stretch Challenge...2nd edit

--- src/bailey/Main.java
+++ src/bailey/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Lee.");
         System.out.println("Everyone helped each other while I was busy with work and other homework.");
+        System.out.println("Now I'm ready, but they are busy.");
     }
 }
