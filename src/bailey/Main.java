44732ad7f9e51532e57ae062b98044185e457c3a
tomtomm <bigdtibbs@gmail.com>
2020-10-09 19:45:04 -0600

    CS246 Week 04 Prove Assignment.

--- src/bailey/Main.java
+++ src/bailey/Main.java
@@ -8,5 +8,8 @@ public class Main {
         System.out.println("Everyone helped each other while I was busy with work and other homework.");
         System.out.println("Now I'm ready, but they are busy.");
         System.out.println("Animal crackers in my soup...");
+        System.out.println("Hello Lee. My name is Daren.");
+        System.out.println("I hope you enjoyed your soup and crackers.");
+        System.out.println("Now this assignment is almost done. Yay! Good luck man.");
     }
 }
